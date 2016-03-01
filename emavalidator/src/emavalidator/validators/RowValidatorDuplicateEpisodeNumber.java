d4257726abbdd3469da19eaa4441784fa53c1c3e
Christopher Kha <christopherkha@gmail.com>
2016-03-01 12:18:55 -0800

    Only check Episode Number duplicates on Episode WorkType.

--- emavalidator/src/emavalidator/validators/RowValidatorDuplicateEpisodeNumber.java
+++ emavalidator/src/emavalidator/validators/RowValidatorDuplicateEpisodeNumber.java
@@ -14,6 +14,7 @@ import emavalidator.columns.SeasonNumber;
 import emavalidator.columns.SeriesTitleInternalAlias;
 import emavalidator.columns.Start;
 import emavalidator.columns.Territory;
+import emavalidator.columns.WorkType;
 import emavalidator.errors.RowErrorDuplicateEpisodeNumber;
 
 public class RowValidatorDuplicateEpisodeNumber extends AbstractRowValidator
@@ -29,49 +30,56 @@ public class RowValidatorDuplicateEpisodeNumber extends AbstractRowValidator
     {
         try
         {
-            String episodeNumber = "";
-            String concatString = "";
-            ArrayList<String> uniqueColumnsArray = new ArrayList<String>();
+            
+            String workType = rowValues.get(WorkType.class.getSimpleName());
+           
+            // Only check for duplicate episode number for WorkType: "Episode".
+            if (WorkType.isEpisode(workType)) {
+                String episodeNumber = "";
+                String concatString = "";
+                ArrayList<String> uniqueColumnsArray = new ArrayList<String>();
 
-            // This RowValidator is only used for TV.
-            // EMA Version 1.6 TV
-            if (this.emaVersion == AbstractEMASpec.EMAVersion.EMASpec16TV) 
-            {
-                // Get all the necessary values from the row to concatenate a UID for Movie.
-                String territory = rowValues.get(Territory.class.getSimpleName());
-                String title = rowValues.get(SeriesTitleInternalAlias.class.getSimpleName());
-                String seasonNumber = rowValues.get(SeasonNumber.class.getSimpleName());
-                episodeNumber = rowValues.get(EpisodeNumber.class.getSimpleName());
-                String licenseType = rowValues.get(LicenseType.class.getSimpleName());
-                String formatProfile = rowValues.get(FormatProfile.class.getSimpleName());
-                String start = rowValues.get(Start.class.getSimpleName());
-                String end = rowValues.get(End.class.getSimpleName());
+                // This RowValidator is only used for TV.
+                // EMA Version 1.6 TV
+                if (this.emaVersion == AbstractEMASpec.EMAVersion.EMASpec16TV) 
+                {
+                    // Get all the necessary values from the row to concatenate a UID for Movie.
+                    String territory = rowValues.get(Territory.class.getSimpleName());
+                    String title = rowValues.get(SeriesTitleInternalAlias.class.getSimpleName());
+                    String seasonNumber = rowValues.get(SeasonNumber.class.getSimpleName());
+                    episodeNumber = rowValues.get(EpisodeNumber.class.getSimpleName());
+                    String licenseType = rowValues.get(LicenseType.class.getSimpleName());
+                    String formatProfile = rowValues.get(FormatProfile.class.getSimpleName());
+                    String start = rowValues.get(Start.class.getSimpleName());
+                    String end = rowValues.get(End.class.getSimpleName());
+                    
+                    String[] array = {territory, title, seasonNumber, episodeNumber, licenseType, formatProfile, start, end};
+                    
+                    for(int i = 0; i < array.length; i++) {
+                        uniqueColumnsArray.add(array[i]);
+                    }
+                }
                 
-                String[] array = {territory, title, seasonNumber, episodeNumber, licenseType, formatProfile, start, end};
+                // Concatenate all values into a Unique ID String
+                concatString = ValidatorUtils.concatUIDString(uniqueColumnsArray);
                 
-                for(int i = 0; i < array.length; i++) {
-                    uniqueColumnsArray.add(array[i]);
+                // Check to make sure the UID String is not already in the static hashmap.
+                if(map.get(concatString) != null) {
+                    //If found, notify that a duplicate was found.
+                    //String episodeNumber = this.map.get(concatString);
+
+                    ErrorLog.appendError(new RowErrorDuplicateEpisodeNumber(rowNumber, 
+                                                RowErrorDuplicateEpisodeNumber.DUPLICATE_EPISODE_FOUND_ERROR_MESSAGE,
+                                                episodeNumber,
+                                                RowErrorDuplicateEpisodeNumber.DUPLICATE_EPISODE_FOUND_ERROR_DETAILS));
+                }
+                
+                // Insert the UID String into the hashmap.
+                if(concatString.length() > 0) {
+                    this.map.put(concatString, episodeNumber);
                 }
             }
             
-            // Concatenate all values into a Unique ID String
-            concatString = ValidatorUtils.concatUIDString(uniqueColumnsArray);
-            
-            // Check to make sure the UID String is not already in the static hashmap.
-            if(map.get(concatString) != null) {
-                //If found, notify that a duplicate was found.
-                //String episodeNumber = this.map.get(concatString);
-
-                ErrorLog.appendError(new RowErrorDuplicateEpisodeNumber(rowNumber, 
-                                            RowErrorDuplicateEpisodeNumber.DUPLICATE_EPISODE_FOUND_ERROR_MESSAGE,
-                                            episodeNumber,
-                                            RowErrorDuplicateEpisodeNumber.DUPLICATE_EPISODE_FOUND_ERROR_DETAILS));
-            }
-            
-            // Insert the UID String into the hashmap.
-            if(concatString.length() > 0) {
-                this.map.put(concatString, episodeNumber);
-            }
             return false;
         } 
         catch (NullPointerException NPE) { return true; }
